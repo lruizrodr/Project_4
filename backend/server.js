302d154df7a032e0be125618dca56bd9abd6b950
lruizrodr <lruiz0002@gmail.com>
2025-11-12 11:46:29 -0600

    readded, removed during troubleshooting

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,17 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { pool } from "./db.js";
 import authRouter from "./routes/auth.js";
+import categoriesRouter from "./routes/categories.js";
+import questionsRouter from "./routes/questions.js";
 
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api", authRouter);
+app.use("/api", authRouter);
+app.use("/api", categoriesRouter);
+app.use("/api", questionsRouter);
 
 app.get("/", (req, res) => {
   res.json({ message: "ok" });
